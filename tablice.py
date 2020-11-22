40a2e6bb0254d51566cd1f25423b408aa9a8395d
Łukasz Frątczak <lukasz_fratczak@wp.pl>
2020-11-22 21:55:24 +0100

    add photos

--- tablice.py
+++ tablice.py
@@ -6,7 +6,7 @@ import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-img = cv2.imread('auto.jpg', cv2.IMREAD_COLOR)
+img = cv2.imread('photos/auto.jpg', cv2.IMREAD_COLOR)
 img = cv2.resize(img, (600, 400))
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
