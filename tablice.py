40a1e3e70c8f7a55339f99f98188a81e09938aac
JanCiechan <64535774+JanCiechan@users.noreply.github.com>
2020-11-25 12:08:32 +0100

    glownie zdjecia

--- tablice.py
+++ tablice.py
@@ -3,16 +3,26 @@ import cv2
 import imutils
 import numpy as np
 import pytesseract
-
+from skimage.segmentation import clear_border
+def locate_license_plate_candidates(gray, keep=5):
+		# perform a blackhat morphological operation that will allow
+		# us to reveal dark regions (i.e., text) on light backgrounds
+		# (i.e., the license plate itself)
+		rectKern = cv2.getStructuringElement(cv2.MORPH_RECT, (13, 5))
+		blackhat = cv2.morphologyEx(gray, cv2.MORPH_BLACKHAT, rectKern)
+		cv2.imshow("Blackhat", blackhat)
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 img = cv2.imread('photos/auto.jpg', cv2.IMREAD_COLOR)
 img = cv2.resize(img, (600, 400))
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
 gray = cv2.bilateralFilter(gray, 13, 15, 15)
 
+
 edged = cv2.Canny(gray, 30, 200)
+
 contours = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 contours = imutils.grab_contours(contours)
 contours = sorted(contours, key=cv2.contourArea, reverse=True)[:100]
@@ -48,6 +58,7 @@ Cropped = gray[topx:bottomx + 1, topy:bottomy + 1]
 text = pytesseract.image_to_string(Cropped, config='--psm 11')
 print("programming_fever's License Plate Recognition\n")
 print("Detected license plate Number is:", text)
+
 img = cv2.resize(img, (500, 300))
 Cropped = cv2.resize(Cropped, (400, 200))
 cv2.imshow('car', img)
